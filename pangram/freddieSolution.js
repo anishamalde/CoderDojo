da6a9419b0ad77af64b7fb6eacca87f00f7902f9
Freddie MacGregor <fm8pref9@gmail.com>
2021-05-21 14:17:15 +0100

    rename file

--- pangram/freddieSolution.js
+++ pangram/freddieSolution.js
@@ -1,3 +1,5 @@
+// Solution using reduce function.
+
 const isLetter = char => /[a-zA-Z]/.test(char);
 // borrowed from this source, works for english alphabet.
 // https://coderrocketfuel.com/article/how-to-check-if-a-character-is-a-letter-using-javascript
